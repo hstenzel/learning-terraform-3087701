9b7c07a2172594aaf66a52d2d1e6983f76930c4e
Harley Stenzel <hstenzel@users.noreply.github.com>
2023-04-06 21:56:22 -0400

    fix subnet and vpc

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ module "vpc" {
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-  
+  subnet_id     = module.vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
@@ -48,7 +48,7 @@ module "blog_sg" {
   name = "blog"
   
   #vpc_id = data.aws_vpc.default.id
-  vpc_id = module.vpc.public_subnets[0]
+  vpc_id = module.vpc.vpc_id
   
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
